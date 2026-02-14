bd84d425ef32765ee20976c4669da5a7a77417a3
Andrei Abena <abenaandrei52@gmail.com>
2026-02-14 10:24:53 +0800

    Update login credentials in login_systems.py

--- login_systems.py
+++ login_systems.py
@@ -1,7 +1,7 @@
 print("Login System")
 print("============")
-correct_user = "admin"
-correct_pass = "12345"
+correct_user = "andreiabena"
+correct_pass = "andrei3455"
 
 username=input("Input Username: "
     )
