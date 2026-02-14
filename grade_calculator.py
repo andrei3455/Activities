bbfe54262f6a3ed9f14bf7554e2eaeccf2f15eec
Andrei Abena <abenaandrei52@gmail.com>
2026-02-14 12:27:45 +0800

    Update grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -9,12 +9,26 @@ print(" ")
 student_score=int(input("Input Score (0-100): "))
 if student_score > 100 or student_score < 0:
     print("Invalid score! Please enter a number between 0 and 100.")
-elif student_score >= 90:
+elif student_score >= 98:
+    print("Grade: A+ - Excellent!")
+elif student_score >= 93:
     print("Grade: A - Excellent!")
-elif student_score >= 80:
+elif student_score >= 90:
+    print("Grade: A- - Excellent!")
+elif student_score >= 87:
+    print("Grade: B+ - Very Good!")
+elif student_score >= 83:
     print("Grade: B - Very Good!")
-elif student_score >= 70:
+elif student_score >= 80:
+    print("Grade: B- - Very Good!")
+elif student_score >= 77:
+    print("Grade: C+ - Good Job!")
+elif student_score >= 73:
     print("Grade: C - Good Job!")
+elif student_score >= 70:
+    print("Grade: C- - Good Job!")
+elif student_score >= 67:
+    print("Grade: D+ - Keep Improving!")
 elif student_score >= 60:
     print("Grade: D - Keep Improving!")
 else:
